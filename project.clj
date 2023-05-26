2c6eac4150190105beaa4ef7b3fdae718ade7054
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2023-05-26 14:35:50 +0200

    Added JFreeChart Dependencies

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
                  [org.clojure/tools.cli           "1.0.194"]
                  [org.apache.commons/commons-math "2.2"]
                  [org.clojure/algo.generic        "0.1.3"]
+		 [org.jfree/jfreechart            "1.5.0"]
                  [seesaw                          "1.5.0"]
                  [reply                           "0.4.4"
                   :exclusions [org.clojure/clojure
--- src/main/clojure/conexp/plot/plot.clj
+++ src/main/clojure/conexp/plot/plot.clj
@@ -1,35 +1,23 @@
-(ns plot
-   (:require 
-    [libpython-clj.python :as py]
-    [tech.v2.datatype :as dtype]
-    [tech.v2.tensor :as dtt]
-    [clojure.java.io :as io]
-    [java.awt.image BufferedImage]
-    [javax.imageio ImageIO]))
-
-
-
-(require '[libpython-clj.python :as py])
-(require '[tech.v2.datatype :as dtype])
-(require '[tech.v2.tensor :as dtt])
-(require '[clojure.java.io :as io])
-(import '[java.awt.image BufferedImage])
-(import '[javax.imageio ImageIO])
-
-
-
-(ns conexp.fca.causal-implications
-  "Causal Implications for Formal Concept Analysis."
+(ns conexp.plot.plot
+  "Plotting Functions for Visualization."
   (:require
    [conexp.base :refer :all]
-   [conexp.io.contexts :refer :all]
-   [conexp.io.fcas :refer :all]
-   [conexp.fca.contexts :refer :all]
-   [clojure.set :as set]))
+   [conexp.fca.many-valued-contexts :refer :all])
+  (:import
+    [java.util Date]
+    [org.jfree.chart ChartFactory ChartUtils JFreeChart]
+    [org.jfree.chart.plot    PlotOrientation]
+    [org.jfree.data.category CategoryDataset DefaultCategoryDataset]
+    [org.jfree.data.general  PieDataset DefaultPieDataset]
+    [org.jfree.data.time     RegularTimePeriod Millisecond Second Minute Hour Day Week Month Quarter Year
+                             TimeSeries TimeSeriesCollection]
+    [org.jfree.data.xy       XYDataset]))
+
+(def ctx (make-mv-context #{"Brainstorm" "Stoneforge Mystic" "Tundra" "Supreme Verdict"}
+                          #{"Type" "CMC" "Copies"}
+                          #{["Brainstorm" "Type" "Instant"] ["Brainstorm" "CMC" 1] ["Brainstorm" "Copies" 4]
+                            ["Stoneforge Mystic" "Type" "Creature"] ["Stoneforge Mystic" "CMC" 2] ["Stoneforge Mystic" "Copies" 4]
+                            ["Tundra" "Type" "Land"] ["Tundra" "CMC" 0] ["Tundra" "Copies" 2]
+                            ["Supreme Verdict" "Type" "Sorcery"] ["Supreme Verdict" "CMC" 4] ["Supreme Verdict" "Copies" 1]}))
 
 
-;;Uncomment this line to load a different version of your python shared library:
-;;(alter-var-root #'libpython-clj.jna.base/*python-library* (constantly "python3.7m"))
-
-
-(py/initialize!)
